593d0b2c85f874c889d94975253955459adf2b80
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2025-06-23 14:02:28 +0200

    Add contact form with EmailJS integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Products from './pages/Products/Products';
-
+import Contact from './pages/Contact/Contact';
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,76 @@
+
+import './Contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+
+function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+        .sendForm('service_2jlcgs9', 'template_x7v4wlt', form.current, 'AUAgybu-2xfeZWk89')
+        .then(
+        (result) => {
+            alert('Message sent!');
+        },
+        (error) => {
+            alert('Failed to send. Please try again.');
+        }
+        );
+    };
+
+  return (
+    <main className="contact">
+      <h2>Contact</h2>
+
+      <section className="contact-info">
+        <p>
+          <strong>📦 Home delivery service:</strong><br />
+          All the city of Stockholm and Malmö (with additional cost).
+        </p>
+
+        <p>
+          <strong>📍 Pickup address:</strong><br />
+          Via la Costa Avenue. Blue Port gated neighbourhood.<br />
+          GPS location will be sent via WhatsApp.
+        </p>
+
+        <p>
+          <strong>📅 Orders:</strong><br />
+          Must be placed 2 days in advance for availability and planning.
+        </p>
+
+        <p>
+          <strong>⏰ Opening hours:</strong><br />
+          Tuesday to Saturday, 9 a.m. to 2 p.m.
+        </p>
+      </section>
+      <section className="contact-form">
+        <h3>Send us a message</h3>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>
+                Your Name:
+                <input type="text" name="user_name" required />
+            </label>
+
+            <label>
+                Your Email:
+                <input type="email" name="user_email" required />
+            </label>
+
+            <label>
+                Message:
+                <textarea name="message" rows="5" required></textarea>
+            </label>
+
+            <button type="submit">Send</button>
+        </form>
+    </section>
+
+    </main>
+  );
+}
+
+export default Contact;
