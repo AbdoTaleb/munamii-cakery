d16ad477c0d8ab7ebf39b99c290586eb1c0b339b
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2025-06-26 11:14:48 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/munamii-cakery/',
+  base: process.env.NODE_ENV === 'production' ? '/munamii-cakery/' : '/',
   plugins: [react()],
 })
