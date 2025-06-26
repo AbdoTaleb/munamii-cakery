1b6f08b3e92f93552dd06996855bc4d41cfcbca3
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2025-06-26 13:21:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (sessionStorage.redirect) {
+  const redirectPath = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, "", redirectPath);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
